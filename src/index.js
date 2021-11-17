af5d1e4372516cfe6dbeb5906f947e47fa1c244d
Tony Nguyen <nhattruong1811@gmail.com>
2021-11-17 21:57:02 +0700

    add provider node

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 // styles
 import 'react-flow-renderer/dist/style.css'
@@ -10,15 +11,28 @@ import DragDrop from './components/DragDrop'
 import UpdatableEdge from './components/UpdatableEdge';
 import Edge from './components/Edge';
 import EdgeWithButtonFlow from './components/EdgeWithButton';
+import ProviderNode from './components/ProviderNode';
 
 function App() {
   return (
-    <>
-      {/* <Edge /> */}
-      {/* <UpdatableEdge /> */}
-      {/* <DragDrop /> */}
-      <EdgeWithButtonFlow />
-    </>
+    <Router>
+      <ul className="nav">
+        <Link to="/">Edge</Link>
+        <Link to="/updatable-edge">Updatable Edge</Link>
+        <Link to="/drag-drop">Drag and Drop</Link>
+        <Link to="/edge-with-button">Edge With Button</Link>
+        <Link to="/provider-node">Provider Node</Link>
+      </ul>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Edge />} />
+          <Route path="/updatable-edge" element={<UpdatableEdge />} />
+          <Route path="/drag-drop" element={<DragDrop />} />
+          <Route path="/edge-with-button" element={<EdgeWithButtonFlow />} />
+          <Route path="/provider-node" element={<ProviderNode />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
