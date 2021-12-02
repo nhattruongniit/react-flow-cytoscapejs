11e0d8e1dd37b85f41274506b77ce0fe2644db67
Tony Nguyen <nhattruong1811@gmail.com>
2021-12-02 22:27:38 +0700

    push code

--- src/components/HTMLForm/ForeName.jsx
+++ src/components/HTMLForm/ForeName.jsx
@@ -4,8 +4,8 @@ function ForeName({ value, setForeName }) {
   return (
     <>
       <h6>FORENAME</h6>
-      <input type="text" value={value} onChange={event => setForeName(event.target.value)} />
-      {/* <iframe src="https://reactjs.org/" width="300px" height="300px" title="123" /> */}
+      {/* <input type="text" value={value} onChange={event => setForeName(event.target.value)} /> */}
+      <iframe src="https://reactjs.org/" width="300px" height="300px" title="123" />
     </>
   )
 }
